2742bf64098f709e98cb0d92cd87b347eaa0aa99
ottojo <jottosmail@gmail.com>
2019-04-15 18:29:22 +0200

    fix pullrequest deployment script

--- deployment/deploy_pullrequest.sh
+++ deployment/deploy_pullrequest.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env bash
-curl -X POST -F "targetSlug=${TRAVIS_REPO_SLUG}" -F "commit=${TRAVIS_PULL_REQUEST_SHA}" -F "pr=${TRAVIS_PULL_REQUEST}" -F "prSlug=${TRAVIS_PULL_REQUEST_SLUG}" http://tbp.ottojo.space:1337
+BUILD_DIR=$(lektor project-info | grep -oP '^Output: \K.+$')
+
+echo $STAGING_DEPLOY_KEY | base64 -d -i > keyfile
+chmod 700 keyfile
+echo uploading to tbp@${STAGING_HOST}:/var/www/${TRAVIS_PULL_REQUEST}
+rsync -p --chmod=ug=rwX,o=rX -e "ssh -i keyfile" -a ${BUILD_DIR}/ tbp@${STAGING_HOST}:/var/www/${TRAVIS_PULL_REQUEST}
+
+rm keyfile
+
+curl -X POST -H "Content-Type: application/json" -H "Authorization: token ${GITHUB_TOKEN}" \
+    --data "{\"body\": \"View this PR on https://${TRAVIS_PULL_REQUEST}.${STAGING_HOST}\"}" \
+    "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+
