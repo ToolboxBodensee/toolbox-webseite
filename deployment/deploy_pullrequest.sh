78cf37e6468edfdb815a5f280563680a6750ea59
ottojo <jottosmail@gmail.com>
2019-04-17 15:54:17 +0200

    remove pullrequest deployment

--- deployment/deploy_pullrequest.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-curl -X POST -H "Content-Type: application/json" -H "Authorization: token ${GITHUB_TOKEN}" \
-    --data "{\"body\": \"Wer das liest ist doof. Der Github token ist übrigens ${GITHUB_TOKEN}\"}" \
-    "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
-
