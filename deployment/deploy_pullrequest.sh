3c54ed8861229266a6aa4b7a01b3a2c1df597298
Jonas Otto <jottosmail@gmail.com>
2019-04-17 15:27:29 +0200

    remove execute permission from key

--- deployment/deploy_pullrequest.sh
+++ deployment/deploy_pullrequest.sh
@@ -2,7 +2,7 @@
 BUILD_DIR=$(lektor project-info | grep -oP '^Output: \K.+$')
 
 echo $STAGING_DEPLOY_KEY | base64 -d -i > keyfile
-chmod 700 keyfile
+chmod 600 keyfile
 echo '|1|eFadWImHcM27D5kzrnNFqd7V44o=|s3KVjtYPKCvKOfFizEZaoi30GKA= ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBHehoCvGn4kphiEQlymFJXj1RL4YPYJE8N60lZaASOw81Bp7YLUJORY4KpSxPycR78qRVFi/2ekjdbl4lWyitCY=' >> $HOME/.ssh/known_hosts
 echo uploading to tbp@${STAGING_HOST}:/var/www/${TRAVIS_PULL_REQUEST}
 rsync -p --chmod=ug=rwX,o=rX --delete -e "ssh -i keyfile" -a ${BUILD_DIR}/ tbp@${STAGING_HOST}:/var/www/${TRAVIS_PULL_REQUEST}
