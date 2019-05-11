85f53d0c89c456fffbcb1ce656b5cb518154d6b1
maxbachmann <44199644+maxbachmann@users.noreply.github.com>
2019-05-11 23:19:43 +0200

    Update staging.sh

--- .travis/staging.sh
+++ .travis/staging.sh
@@ -12,5 +12,5 @@ git commit -m "staging commit"
 git remote add origin https://$LEKTOR_STAGING_KEY@github.com/maxbachmann-staging/website-staging$(($TRAVIS_PULL_REQUEST % 5)).git
 git push --force origin master
 curl -H "Authorization: token $LEKTOR_STAGING_KEY" -X POST \
--d "{\"body\": \"View this PR on https://staging$(($TRAVIS_PULL_REQUEST % 5)).maxbachmann.de\"}" \
+-d "{\"body\": \"View this PR on https://staging$(($TRAVIS_PULL_REQUEST % 5)).maxbachmann.de the compiled sources with scss compiled to minified css and minified js can be found at https://github.com/maxbachmann-staging/website-staging$(($TRAVIS_PULL_REQUEST % 5)) \"}" \
 "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
