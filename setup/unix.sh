1dcbac41de827e305278e0a8e901e89d07c74beb
maxbachmann <kontakt@maxbachmann.de>
2019-06-17 16:08:55 +0200

    add missing sudo

--- setup/unix.sh
+++ setup/unix.sh
@@ -1,12 +1,9 @@
 if hash apt-get 2>/dev/null; then
-  apt-get update -qq >/dev/null && apt-get install -qq apt-utils imagemagick python3-pip python3-setuptools build-essential
+  sudo apt-get update -qq >/dev/null && sudo apt-get install -qq apt-utils imagemagick python3-pip python3-setuptools build-essential
 elif hash pacman 2>/dev/null; then
-  pacman -Syu imagemagick python-pip glibc lib32-glibc base-devel --noconfirm
+  sudo pacman -Syu imagemagick python-pip glibc lib32-glibc base-devel --noconfirm
 elif hash dnf 2>/dev/null; then
-  dnf install -y ImageMagick python3-pip @development-tools
-# elif hash brew 2>/dev/null; then
-#  brew list imagemagick || brew install imagemagick
-#  brew list python3 || brew install python3
+  sudo dnf install -y ImageMagick python3-pip @development-tools
 else 
   echo -e "Please install Imagemagick and python3-pip "
 fi
