3987ffd04383b2d7eaa9bb9fd0a98651fa81bbb7
Jonas Hess <jonas.hess@mailbox.org>
2020-01-18 19:00:26 +0100

    ++ added viewport recognition and depending on that another default view for the calendar

--- assets/js/toolboxcalendar.js
+++ assets/js/toolboxcalendar.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
-    $('#calendar').fullCalendar({
-      height: 1000,
-      defaultView: 'month',
-      timezone: 'local',
-      weekends: true
-    });
-    $('#calendar').fullCalendar('addEventSource', ical_event_source('https://toolbox-bodensee.de/termine.ics', 'green', ''));
-    setInterval(() => $('#calendar').fullCalendar('refetchEvents'), 5 * 60 * 1000);
-    setInterval(() => $('#calendar').fullCalendar('gotoDate', moment()), 30 * 60 * 1000);
+  var currentView = 'month';
+  if ($(window).width() <= 480) {
+    currentView = 'listWeek';
+  }
+  $('#calendar').fullCalendar({
+    height: 1000,
+    defaultView: currentView,
+    timezone: 'local',
+    weekends: true
+  });
+  $('#calendar').fullCalendar('addEventSource', ical_event_source('/termine.ics', 'green', ''));
+  setInterval(() => $('#calendar').fullCalendar('refetchEvents'), 5 * 60 * 1000);
+  setInterval(() => $('#calendar').fullCalendar('gotoDate', moment()), 30 * 60 * 1000);
 });
