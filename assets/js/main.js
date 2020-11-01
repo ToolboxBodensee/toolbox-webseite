62721c101a33f2a8b89e144d4b3388f0bc9d10af
David Blumer <hallo@davidblumer.de>
2020-11-01 13:49:38 +0000

    Space-Status closed again

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,27 +6,31 @@
 */
 
 function loadSpaceStatus() {
-  $.getJSON("/toolboxbodensee.json")
-   .done(function(data) {
-    if (data.state.open === true) {
-       $('.space-opened').css("display", "block");
-       $('.space-closed').hide();
-       $('.space-unknown').hide();
-     } else if (data.state.open === false) {
-       $('.space-opened').hide();
-       $('.space-closed').css("display", "block");
-       $('.space-unknown').hide();
-     } else {
-       $('.space-opened').hide();
-       $('.space-closed').hide();
-       $('.space-unknown').css("display", "block");
-     }
-   })
-   .fail(function(xhr, status, error) {
-     $('.space-opened').hide();
-     $('.space-closed').hide();
-     $('.space-unknown').css("display", "block");
-   });
+  $('.space-opened').hide();
+  $('.space-closed').css("display", "block");
+  $('.space-unknown').hide();
+
+  // $.getJSON("/toolboxbodensee.json")
+  //  .done(function(data) {
+  //   if (data.state.open === true) {
+  //      $('.space-opened').css("display", "block");
+  //      $('.space-closed').hide();
+  //      $('.space-unknown').hide();
+  //    } else if (data.state.open === false) {
+  //      $('.space-opened').hide();
+  //      $('.space-closed').css("display", "block");
+  //      $('.space-unknown').hide();
+  //    } else {
+  //      $('.space-opened').hide();
+  //      $('.space-closed').hide();
+  //      $('.space-unknown').css("display", "block");
+  //    }
+  //  })
+  //  .fail(function(xhr, status, error) {
+  //    $('.space-opened').hide();
+  //    $('.space-closed').hide();
+  //    $('.space-unknown').css("display", "block");
+  //  });
 }
 
 function initSpaceStatus() {
